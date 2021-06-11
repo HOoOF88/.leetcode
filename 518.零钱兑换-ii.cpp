3cd12db79a3473c798d2700c52ad2bffa845d354
HOoOF88 <hf904178460@gmail.com>
2021-06-11 23:33:47 +0800

    Create 518.零钱兑换-ii.cpp

---
+++ 518.零钱兑换-ii.cpp
@@ -0,0 +1,33 @@
+// @before-stub-for-debug-begin
+
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=518 lang=cpp
+ *
+ * [518] 零钱兑换 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int a[amount+1] ;
+        memset(a,0,sizeof(a));
+        a[0] = 1;
+
+        for(int j = 0;j<coins.size();j++)
+        {
+            for(int i=1;i<=amount;i++)
+            {
+                if(i>=coins[j])
+                {
+                    a[i] += a[i-coins[j]];
+                }
+            }
+            
+        }
+        return a[amount];
+    }
+};
+// @lc code=end
